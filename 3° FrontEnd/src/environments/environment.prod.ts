d3c7166a85f42ed6c3a14e8c14c5301373316f11
wellingtonPLF <wellplf@gmail.com>
2025-03-09 19:19:58 -0300

    Changing environment.prod.ts

--- 3° FrontEnd/src/environments/environment.prod.ts
+++ 3° FrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+import { LocalStorageService } from "src/app/shared/service/local-storage.service";
+
+export let protocol = 'https://'
+let baseURL = "http://localhost:8080" //import.meta.env.NG_APP_API_URL
+const hostname = new LocalStorageService().getToken('backendHostname');
+
 export const environment = {
   production: true,
-  apiUrl: import.meta.env.NG_APP_API_URL
+  apiUrl: hostname ? (hostname.includes(protocol) ? hostname : baseURL) : baseURL
 };
